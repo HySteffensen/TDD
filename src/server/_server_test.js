f62299ce2867bcdb110989276119636e6d61c0d9
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-29 04:20:26 -0700

    EP 12 created first server test

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,10 +2,25 @@
 
 var assert = require("chai").assert;
 var server = require("./server");
+var http = require("http");
 
 
-describe("Addition", function() {
-    it("should equal", function() {
-        assert.equal(server.number(), 3);
+describe("Server", function() {
+
+    before(function() {
+        server.start();
+    });
+
+    after(function() {
+        server.stop(function() {
+            console.log("stop callback");
+        });
+    });
+
+    it("should get a 200 response", function() {
+        http.get("http://localhost:8080", function(res) {
+            // res.on("data", function(){});
+            assert.equal(res.statusCode, 200);
+        });
     });
 });
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,21 @@
 "use strict";
 
-exports.number = function() {
-    return 3;
+var http = require("http");
+
+var server;
+
+exports.start = function() {
+    server = http.createServer();
+
+    server.on("request", function(req, res) {
+        res.end();
+    });
+
+    server.listen(8080);
+    console.log("server started");
+};
+
+exports.stop = function(callback) {
+    server.close(callback);
+    console.log("server stopped");
 };
